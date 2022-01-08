52584b81695c6fc699c527c2a8a95fcf3bba09be
chinu1994 <sachin2308>
2022-01-09 04:46:51 +0530

    all points done

--- sltech_custom_accounting_module/models/account_move.py
+++ sltech_custom_accounting_module/models/account_move.py
@@ -38,7 +38,8 @@ class AccountInvoice(models.Model):
 
     sltech_move_line_entries = fields.One2many('account.move.line', 'sltech_move_id')
 
-    sltech_move_id = fields.Many2one('account.move')  # to store third party move line entries   ====>   totally seperate and independent of other fields
+    sltech_move_ids = fields.Many2many('account.move', 'sltech_account_move_move_rel', 'c1', 'c2')  # to store third party move line entries   ====>   totally seperate and independent of other fields
+    sltech_count = fields.Integer(default=0)
 
     def sltech_button_create_landed_costs(self):
         """Create a `stock.landed.cost` record associated to the account move of `self`, each
@@ -106,34 +107,36 @@ class AccountInvoice(models.Model):
 
     def sltech_action_post(self):
         if self.sltech_move_line:
-            if not self.sltech_move_id:
 
-                self.sltech_move_id = self.create({
-                    'partner_id': self.partner_id.id,
-                    'name': self.name+'-Landed Cost',
-                    'date': self.date,
-                    'journal_id': self.journal_id.id,
-                    'currency_id': self.currency_id.id,
-                }).id
-
-            line_ids = []
+            sltech_move_ids = {}
             for line in self.sltech_move_line:
+                line_ids = []
                 if not line.product_id.seller_ids:
                     raise ValidationError('Please choose Vendor inside Landed Cost in %s!'%line.product_id.name)
+
+                partner_landed_id = line.product_id.seller_ids[0].name
+
+                # check for landed costs having same seller
+                if not sltech_move_ids.get(partner_landed_id.id):
+                    sltech_move_ids[partner_landed_id.id] = []
 
                 line_ids.append((0, 0, {
                     'account_id': line.product_id.additional_account_id.id,
                     'name': line.product_id.name,
                     'debit': line.price_untax,
                     'sltech_move_id': self.id,
+
+                    'exclude_from_invoice_tab': True,
                 }))
 
                 line_ids.append((0, 0, {
-                    'account_id': line.product_id.seller_ids[0].name.property_account_payable_id.id,
-                    'name': line.product_id.seller_ids[0].name.property_account_payable_id.name,
+                    'account_id': partner_landed_id.property_account_payable_id.id,
+                    'name': partner_landed_id.property_account_payable_id.name,
                     'credit': line.price_subtotal,
                     'sltech_move_id': self.id,
-                    'partner_id': line.product_id.seller_ids[0].name.id,
+                    'partner_id': partner_landed_id.id,
+
+                    'exclude_from_invoice_tab': True,
                 }))
 
                 account_tax = line.tax_ids.compute_all(price_unit=line.amount_tax,
@@ -145,46 +148,58 @@ class AccountInvoice(models.Model):
                     'name': account_tax['taxes'][0]['name'],
                     'debit': line.amount_tax,
                     'sltech_move_id': self.id,
+
+                    'exclude_from_invoice_tab': True,
                 })]
 
-            # line_ids = [(0, 0, {
-            #     'account_id': line.product_id.additional_account_id.id,
-            #     'name': line.product_id.name,
-            #     'debit': line.price_untax,
-            #     'sltech_move_id': self.id,
-            # }) for line in self.sltech_move_line] + [(0, 0, {
-            #     'account_id': line.product_id.seller_ids[0].name.property_account_payable_id.id,
-            #     'name': line.product_id.seller_ids[0].name.property_account_payable_id.name,
-            #     'credit': line.price_subtotal,
-            #     'sltech_move_id': self.id,
-            # }) for line in self.sltech_move_line]
-            # if self.sltech_amount_tax:
-            #     account_tax = account_tax.compute_all(price_unit=self.sltech_amount_tax,
-            #                                           currency=False,
-            #                                           quantity=1)
-            #
-            #
-            #     line_ids += [(0, 0, {
-            #         'account_id': account_tax['taxes'][0]['account_id'],
-            #         'name': account_tax['taxes'][0]['name'],
-            #         'debit': self.sltech_amount_tax,
-            #         'sltech_move_id': self.id,
-            #     })]
+                sltech_move_ids[partner_landed_id.id] += line_ids
 
-            self.sltech_move_id.write({
-                'line_ids': [(5, 0)]
-            })
-            self.sltech_move_id.write({
-                'line_ids': line_ids
-            })
 
-            self.sltech_move_id.action_post()
+            already_existing_partner_landed_ids = [x.partner_id.id for x in self.sltech_move_ids]
+            for partner_landed_id in sltech_move_ids:
+                if partner_landed_id not in already_existing_partner_landed_ids:
+                    temp_move_id = self.create({
+                        'partner_id': partner_landed_id,
+                        # 'name': self.name + '-Landed Cost-' + str(self.sltech_count),
+                        'ref': self.name + '-Landed Cost-' + str(self.sltech_count),
+                        'date': self.date,
+                        'line_ids': sltech_move_ids[partner_landed_id],
+                        'journal_id': self.journal_id.id,
+                        'currency_id': self.currency_id.id,
+                    })
+                    self.sltech_move_ids = [(4, temp_move_id.id)]
+                    temp_move_id.action_post()
+
+                else:
+                    exist_move_id = [x for x in self.sltech_move_ids if x.partner_id.id == partner_landed_id]
+                    if exist_move_id:
+                        exist_move_id = exist_move_id[0]
+                        exist_move_id.write({
+                            'line_ids': [(5, 0)]
+                        })
+                        if sltech_move_ids[partner_landed_id]:
+                            exist_move_id.write({
+                                'line_ids': sltech_move_ids[partner_landed_id],
+                            })
+                self.sltech_count += 1
+
+            li1 = list(sltech_move_ids.keys())
+            li2 = already_existing_partner_landed_ids
+
+            difference_lists = list(set(li1) - set(li2)) + list(set(li2) - set(li1))
+
+
+            for sltech_move_id in self.sltech_move_ids :
+                if sltech_move_id.partner_id.id not in difference_lists and sltech_move_id.line_ids:
+                    sltech_move_id.action_post()
 
     def button_draft(self):
         res = super(AccountInvoice, self).button_draft()
 
-        if self.sltech_move_id:
-            self.sltech_move_id.button_draft()
+        # if self.sltech_move_id:
+        #     self.sltech_move_id.button_draft()
+        for sltech_move_id in self.sltech_move_ids:
+            sltech_move_id.button_draft()
 
         return res
 
@@ -220,7 +235,8 @@ class SLTECHAccountMoveLine(models.Model):
     @api.onchange('product_id', 'tax_ids')
     def onchange_pro_id(self):
         if self.product_id:
-            self.price_unit = self.product_id.list_price
+            if self.price_unit == 0:
+                self.price_unit = self.product_id.list_price
             self.quantity = 1
 
             tax_details = self.tax_ids.compute_all(price_unit=self.price_unit, currency=False, quantity=self.quantity, product=self.product_id)
