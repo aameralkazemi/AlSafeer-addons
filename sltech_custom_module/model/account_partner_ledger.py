df6e39594f4aa6b7858c5958eb2c214e826e4930
Amer Alkazemi <potatobo3@gmail.com>
2022-02-05 14:30:58 +0300

    added app to enable debug mode

--- sltech_custom_module/model/account_partner_ledger.py
+++ sltech_custom_module/model/account_partner_ledger.py
@@ -21,7 +21,7 @@ class ReportPartnerLedger(models.AbstractModel):
             {'name': self.format_value(credit), 'class': 'number'},
         ]
         if self.user_has_groups('base.group_multi_currency'):
-            columns.append({'name': ''})
+            columns.append({'name': self.format_value(credit)})
         columns.append({'name': self.env.user.company_id.currency_id.symbol})
         columns.append({'name': self.format_value(balance), 'class': 'number'})
 
@@ -37,61 +37,6 @@ class ReportPartnerLedger(models.AbstractModel):
         }
 
     @api.model
-    def _get_report_line_move_line(self, options, partner, aml, cumulated_init_balance, cumulated_balance):
-        if aml['payment_id']:
-            caret_type = 'account.payment'
-        elif aml['move_type'] in ('in_refund', 'in_invoice', 'in_receipt'):
-            caret_type = 'account.invoice.in'
-        elif aml['move_type'] in ('out_refund', 'out_invoice', 'out_receipt'):
-            caret_type = 'account.invoice.out'
-        else:
-            caret_type = 'account.move'
-
-        date_maturity = aml['date_maturity'] and format_date(self.env, fields.Date.from_string(aml['date_maturity']))
-        columns = [
-            {'name': aml['journal_code']},
-            {'name': aml['account_code']},
-            {'name': self._format_aml_name(aml['name'], aml['ref'], aml['move_name'])},
-            {'name': date_maturity or '', 'class': 'date'},
-            {'name': aml['full_rec_name'] or ''},
-            {'name': self.format_value(cumulated_init_balance), 'class': 'number'},
-            {'name': self.format_value(aml['debit'], blank_if_zero=True), 'class': 'number'},
-            {'name': self.format_value(aml['credit'], blank_if_zero=True), 'class': 'number'},
-        ]
-        if self.user_has_groups('base.group_multi_currency'):
-            if aml['currency_id']:
-                currency = self.env['res.currency'].browse(aml['currency_id'])
-                formatted_amount = self.format_value(aml['amount_currency'], currency=currency, blank_if_zero=True)
-                columns.append({'name': formatted_amount, 'class': 'number'})
-            else:
-                columns.append({'name': ''})
-        columns.append({'name': self.env.user.company_id.currency_id.symbol})
-        columns.append({'name': self.format_value(cumulated_balance), 'class': 'number'})
-        return {
-            'id': aml['id'],
-            'parent_id': 'partner_%s' % partner.id,
-            'name': format_date(self.env, aml['date']),
-            'class': 'date',
-            'columns': columns,
-            'caret_options': caret_type,
-            'level': 4,
-        }
-
-    @api.model
-    def _get_report_line_load_more(self, options, partner, offset, remaining, progress):
-        return {
-            'id': 'loadmore_%s' % partner.id,
-            'offset': offset,
-            'progress': progress,
-            'remaining': remaining,
-            'class': 'o_account_reports_load_more text-center',
-            'parent_id': 'account_%s' % partner.id,
-            'name': _('Load more... (%s remaining)' % remaining),
-            'colspan': 11 if self.user_has_groups('base.group_multi_currency') else 10,
-            'columns': [{}],
-        }
-
-    @api.model
     def _get_report_line_total(self, options, initial_balance, debit, credit, balance):
         columns = [
             {'name': self.format_value(initial_balance), 'class': 'number'},
@@ -99,7 +44,7 @@ class ReportPartnerLedger(models.AbstractModel):
             {'name': self.format_value(credit), 'class': 'number'},
         ]
         if self.user_has_groups('base.group_multi_currency'):
-            columns.append({'name': ''})
+            columns.append({'name': self.format_value(credit)})
         columns.append({'name': self.env.user.company_id.currency_id.symbol})
         columns.append({'name': self.format_value(balance), 'class': 'number'})
         return {
@@ -110,22 +55,3 @@ class ReportPartnerLedger(models.AbstractModel):
             'columns': columns,
             'colspan': 6,
         }
-
-    def _get_columns_name(self, options):
-        columns = [
-            {},
-            {'name': _('JRNL')},
-            {'name': _('Account')},
-            {'name': _('Ref')},
-            {'name': _('Due Date'), 'class': 'date'},
-            {'name': _('Matching Number')},
-            {'name': _('Initial Balance'), 'class': 'number'},
-            {'name': _('Debit'), 'class': 'number'},
-            {'name': _('Credit'), 'class': 'number'}]
-
-        if self.user_has_groups('base.group_multi_currency'):
-            columns.append({'name': _('Amount Currency'), 'class': 'number'})
-        columns.append({'name': _('Currency')})
-        columns.append({'name': _('Balance'), 'class': 'number'})
-
-        return columns
