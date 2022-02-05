d5133d924a6eec09b0713d001cd463d13afe9125
Amer Alkazemi <potatobo3@gmail.com>
2022-02-05 19:51:03 +0300

    temp solution : removed partner ledger customiztion

--- sltech_custom_module/model/__init__.py
+++ sltech_custom_module/model/__init__.py
@@ -19,4 +19,4 @@
 #
 ##############################################################################
 from . import sale_order_line
-from . import account_partner_ledger
+# from . import account_partner_ledger
